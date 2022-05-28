86851b1a67196187d4e566d3c2ec4a7df2f5aa7e
Sotiris Tavianatos <50800600+swtos7@users.noreply.github.com>
2022-05-28 16:01:25 +0300

    Add files via upload

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -48,5 +49,14 @@ class RouteServiceProvider extends ServiceProvider
         RateLimiter::for('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
         });
+
+        if (Auth::check()) {
+            RateLimiter::for('comment', function (Request $request) {
+                return Limit::perMinute(60)->by($request->user()->id);
+            });
+            }
+        else  
+            return redirect('/login');
+      
     }
 }
