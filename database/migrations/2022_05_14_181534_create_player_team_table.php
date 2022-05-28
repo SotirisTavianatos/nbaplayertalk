4acfe20e689492f517c75d299f4a571c2df8e68a
Sotiris Tavianatos <50800600+swtos7@users.noreply.github.com>
2022-05-28 15:54:09 +0300

    Add files via upload

---
+++ database/migrations/2022_05_14_181534_create_player_team_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('player_team', function (Blueprint $table) {
+            $table->id();
+            $table->integer('player_id');
+            $table->integer('team_id');
+            $table->year('until');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('player_team');
+    }
+};
