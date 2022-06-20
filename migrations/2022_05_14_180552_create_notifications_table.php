a50f1d8697ac34ed7b1eb78f780893c90c1fc393
Sotiris Tavianatos <50800600+SotirisTavianatos@users.noreply.github.com>
2022-06-20 11:00:55 +0300

    Update 2022_05_14_180552_create_notifications_table.php

--- migrations/2022_05_14_180552_create_notifications_table.php
+++ migrations/2022_05_14_180552_create_notifications_table.php
@@ -15,6 +15,10 @@ return new class extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->id();
+            $table->string('message');
+            $table->enum('status',['NEW','SENT','READ']);
+            $table->index("user_id");
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->integer('comments_id');
             $table->timestamps();
         });
